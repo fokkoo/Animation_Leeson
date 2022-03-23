cc170112fe7b1c146fa444908a8fdea32feee12d
KazanskiiPN <fokkoo@yandex.ru>
2022-03-23 13:39:41 +0300

    start Project
    Button added
    Picture added
    Button listner added
    Transition animation added

---
+++ app/src/main/java/com/example/a1lesson5/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.a1lesson5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.TransitionManager
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageButton.setOnClickListener {
+            if (editTextPersonName.text.isEmpty()){
+                textView.setText("Hello no name Cat")
+            }
+            else{
+                textView.setText("Hello "+editTextPersonName.text)
+
+                TransitionManager.beginDelayedTransition(linerLayout)
+                linerLayout.isVisible = !linerLayout.isVisible
+        }
+
+
+        }
+    }
+}
