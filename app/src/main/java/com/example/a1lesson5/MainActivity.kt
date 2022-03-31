c281dadd88403b84389714e32218d1e576ce2e92
KazanskiiPN <fokkoo@yandex.ru>
2022-03-31 11:46:21 +0300

    code is cleaned. layout looks more beautiful

--- app/src/main/java/com/example/a1lesson5/MainActivity.kt
+++ app/src/main/java/com/example/a1lesson5/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.a1lesson5
 
+import android.animation.Animator
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.ChangeBounds
@@ -17,20 +19,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         val hideTextViewSampleTransition by lazy {
+        val hideTextViewSampleTransition by lazy {
             val transitionSet = TransitionSet()
-             transitionSet.duration = 50000 // duration for test, i know it should be less then 300
-             transitionSet.ordering = TransitionSet.ORDERING_TOGETHER
+            transitionSet.duration = 50000 // duration for test, i know it should be less then 300
+            transitionSet.ordering = TransitionSet.ORDERING_TOGETHER
 
-             transitionSet.addTransition(Fade(Fade.OUT))
-             transitionSet.addTransition(ChangeBounds())
-             transitionSet
-
+            transitionSet.addTransition(Fade(Fade.OUT))
+            transitionSet.addTransition(ChangeBounds())
+            transitionSet
         }
 
         val showTextViewSampleTransition by lazy {
@@ -39,15 +39,14 @@ class MainActivity : AppCompatActivity() {
             TransitionSet().ordering = TransitionSet.ORDERING_TOGETHER
             TransitionSet().addTransition(Fade(Fade.IN))
             TransitionSet().addTransition(ChangeBounds())
-
         }
 
-        button_transition_animation.setOnClickListener{
+        button_transition_animation.setOnClickListener {
             val constraintSetSampleTextViewIsVisible = linerLayout.isVisible
 
-            val transitionSet = if (constraintSetSampleTextViewIsVisible){
+            val transitionSet = if (constraintSetSampleTextViewIsVisible) {
                 hideTextViewSampleTransition
-            } else{
+            } else {
                 showTextViewSampleTransition
             }
 
@@ -57,25 +56,19 @@ class MainActivity : AppCompatActivity() {
         }
 
         imageButton.setOnClickListener {
-            if (editTextPersonName.text.isEmpty()){
+            if (editTextPersonName.text.isEmpty()) {
                 textView.setText("Hello no name Cat")
+            } else {
+                textView.setText("Hello " + editTextPersonName.text)
             }
-            else{
-                textView.setText("Hello "+editTextPersonName.text)
-
-
-        }
-
-
         }
 
 
         //отработка нажатия кнопки object_animation , класс который наследуется от value_animation
 
         button_object_animation_alfa.setOnClickListener {
-           val animator = ObjectAnimator.ofFloat(linerLayout, View.ALPHA,0f,1f)
-          //  val animator = ObjectAnimator.ofArgb(Color.BLACK,Color.RED) // не работает
-
+            val animator = ObjectAnimator.ofFloat(linerLayout, View.ALPHA, 0f, 1f)
+            //  val animator = ObjectAnimator.ofArgb(Color.BLACK,Color.RED) // не работает
 
             textView.setText("Hello button_object_animation ")
             animator.duration = 300
@@ -88,7 +81,7 @@ class MainActivity : AppCompatActivity() {
         //отработка нажатия кнопки object_animation rotation
         button_object_animation_rotation.setOnClickListener {
 
-            val animator = ObjectAnimator.ofFloat(linerLayout, View.ROTATION,0f,365f)
+            val animator = ObjectAnimator.ofFloat(linerLayout, View.ROTATION, 0f, 365f)
 
             textView.setText("Hello button_object_animation_rotation ")
             animator.duration = 1000
@@ -101,7 +94,7 @@ class MainActivity : AppCompatActivity() {
 
 
         //отработка нажатия кнопки value_animation , когда используется какое то условие
-         fun createAlphaValueAnimator(): ValueAnimator {
+        fun createAlphaValueAnimator(): ValueAnimator {
             val animator = ValueAnimator.ofFloat(0f, 1f)
 
             animator.duration = 300
@@ -112,32 +105,32 @@ class MainActivity : AppCompatActivity() {
                 val value = valueAnimator.animatedValue as Float
                 textView5.alpha = value
                 textView4.alpha = value
-                linerLayout.alpha=value
-                editTextPersonName.alpha=value
+                linerLayout.alpha = value
+                editTextPersonName.alpha = value
             }
-        return animator
+            return animator
         }
 
-         fun createAlfaAnimator(): ValueAnimator? {
-             val animator = ValueAnimator.ofFloat(0f, 1f)
+        fun createAlfaAnimator(): ValueAnimator? {
+            val animator = ValueAnimator.ofFloat(0f, 1f)
 
-             animator.duration = 1000
-             animator.repeatMode = ValueAnimator.REVERSE
-             animator.repeatCount = 5
-             animator.interpolator = DecelerateInterpolator()
+            animator.duration = 1000
+            animator.repeatMode = ValueAnimator.REVERSE
+            animator.repeatCount = 5
+            animator.interpolator = DecelerateInterpolator()
 
-             animator.addUpdateListener { valueAnimator ->
-                 val value = valueAnimator.animatedValue as Float
-                 textView5.alpha = value
-                 textView4.alpha = value
-                 linerLayout.alpha=value
-                 editTextPersonName.alpha=value
-             }
-             return animator
+            animator.addUpdateListener { valueAnimator ->
+                val value = valueAnimator.animatedValue as Float
+                textView5.alpha = value
+                textView4.alpha = value
+                linerLayout.alpha = value
+                editTextPersonName.alpha = value
+            }
+            return animator
         }
 
         fun createRotationAnimator(): ValueAnimator? {
-            val animator = ObjectAnimator.ofFloat(linerLayout, View.ROTATION,0f,365f)
+            val animator = ObjectAnimator.ofFloat(linerLayout, View.ROTATION, 0f, 365f)
 
             animator.duration = 1000
             animator.repeatMode = ValueAnimator.REVERSE
@@ -148,34 +141,32 @@ class MainActivity : AppCompatActivity() {
                 val value = valueAnimator.animatedValue as Float
                 textView5.alpha = value
                 textView4.alpha = value
-                linerLayout.alpha=value
-                editTextPersonName.alpha=value
+                linerLayout.alpha = value
+                editTextPersonName.alpha = value
             }
             return animator
         }
 
 
         //playing together. the change of list chages the resault
-        button_object_animation_alfa_and_rotation.setOnClickListener{
+        button_object_animation_alfa_and_rotation.setOnClickListener {
             val animatorSet = AnimatorSet()
             val animators = listOf(
 
-                    createRotationAnimator(),
-                    createAlfaAnimator(),
+                createRotationAnimator(),
+                createAlfaAnimator(),
 
-            )
+                )
             animatorSet.playTogether(animators)
             animatorSet.start()
         }
 
 
-        // перемещение объектов
+        // перемещение объектов каждого по отдельности чтобы не запутаться в двух разных вью
 
         var push_animation_number = true;
 
-        button_push_animation.setOnClickListener{
-
-
+        button_push_animation.setOnClickListener {
 
             TransitionManager.beginDelayedTransition(constraint_layout_animation)
 
@@ -183,59 +174,78 @@ class MainActivity : AppCompatActivity() {
             constraintSet.clone(constraint_layout_animation)
 
             val parentId = R.id.button_push_animation
-          //  val dailyImageViewId = R.id.imageView2
-         //   val animatorSampleTextViewId = R.id.textView
+            //  val dailyImageViewId = R.id.imageView2
+            //   val animatorSampleTextViewId = R.id.textView
 
-            if (push_animation_number == true){
+            if (push_animation_number == true) {
                 textView.setText("Hello true")
-                push_animation_number= false;
-                constraintSet.connect(R.id.imageButton,ConstraintSet.BOTTOM,R.id.button_push_animation,ConstraintSet.BOTTOM)
-            } else{
+                push_animation_number = false;
+                constraintSet.connect(
+                    R.id.imageButton,
+                    ConstraintSet.BOTTOM,
+                    R.id.button_push_animation,
+                    ConstraintSet.BOTTOM
+                )
+            } else {
                 textView.setText("Hello false")
-                push_animation_number= true;
-                constraintSet.connect(R.id.imageButton,ConstraintSet.BOTTOM,R.id.button_transition_animation,ConstraintSet.BOTTOM)
+                push_animation_number = true;
+                constraintSet.connect(
+                    R.id.imageButton,
+                    ConstraintSet.BOTTOM,
+                    R.id.button_transition_animation,
+                    ConstraintSet.BOTTOM
+                )
             }
-          // constraintSet.connect(R.id.imageButton,ConstraintSet.BOTTOM,parentId,ConstraintSet.BOTTOM)
-            //constraintSet.connect(animatorSampleTextViewId,ConstraintSet.TOP,dailyImageViewId,ConstraintSet.BOTTOM)
-          // constraintSet.connect(R.id.imageButton,ConstraintSet.TOP,animatorSampleTextViewId,ConstraintSet.BOTTOM)
+
 
             constraintSet.applyTo(constraint_layout_animation)
         }
-
 
 
         // view property animator наиболее лаконичный способ
         button_view_property_animation.setOnClickListener {
             textView.setText("Hello button_view_proprty_animation ")
 
-            if (push_animation_number == true){
+            if (push_animation_number == true) {
                 textView.setText("Hello true")
-                push_animation_number= false;
+                push_animation_number = false;
                 val animator = linerLayout.animate().alpha(0f)
 
                 animator.duration
                 animator.start()
-            } else{
+            } else {
                 textView.setText("Hello false")
-                push_animation_number= true;
+                push_animation_number = true;
                 val animator = linerLayout.animate().alpha(1f)
                 animator.duration
                 animator.start()
             }
 
-
-
-
         }
 
+        button_ARGB_animation.setOnClickListener {
+            val animator = createArgbAnimator()
+            animator.start()
+
+        }
 
         button_value_animation.setOnClickListener {
             textView.setText("Hello button_value_animation ")
             createAlphaValueAnimator()
 
         }
+    }
 
+    private fun createArgbAnimator(): Animator {
+        val animator = ValueAnimator.ofArgb(Color.BLACK, Color.RED)
+        animator.repeatMode = ValueAnimator.REVERSE
+        animator.repeatCount = ValueAnimator.INFINITE
+        animator.duration = 1000
 
-
+        animator.addUpdateListener { valueAnimator ->
+            val value = valueAnimator.animatedValue as Int
+            textView.setTextColor(value)
+        }
+        return animator
     }
 }
